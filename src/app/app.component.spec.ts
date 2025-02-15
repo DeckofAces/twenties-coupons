4cdaf055d300ebc589c6af9c9a5f9f4df4f09c2d
DeckofAces <52782439+DeckofAces@users.noreply.github.com>
2025-02-15 12:36:08 +0100

    update http client testing provider

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,16 +1,27 @@
-import { TestBed } from '@angular/core/testing';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+  let compiled: HTMLElement;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent],
+      declarations: [AppComponent],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA], // This allows unknown elements
     }).compileComponents();
   });
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+    compiled = fixture.nativeElement;
+  });
+
+  it('should create the app component', () => {
+    expect(component).toBeTruthy();
   });
 });
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,30 +3,25 @@ import { provideRouter } from '@angular/router';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { providePrimeNG } from 'primeng/config';
 import { provideHttpClient } from '@angular/common/http';
-import Aura from '@primeng/themes/aura';
 import { MyPreset } from './mypreset';
 
 import { routes } from './app.routes';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideHttpClient(),
+    provideHttpClientTesting(),
     provideAnimationsAsync(),
     providePrimeNG({
       theme: {
         preset: MyPreset,
         options: {
-          darkModeSelector: false
-        }
-      }
-      // theme: {
-      //   preset: Aura,
-      //   options: {
-      //     darkModeSelector: false,
-      //   }
-      // }
-    })
+          darkModeSelector: false,
+        },
+      },
+    }),
   ],
 };
